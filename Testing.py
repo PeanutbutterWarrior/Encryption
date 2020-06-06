f5d0a3a02b12fad2244ce126e34e6678f67d603f
PeanutbutterWarrior <64+PeanutbutterWarrior@users.noreply.github.com>
2020-06-06 12:29:44 +0100

    Testing

--- Testing.py
+++ Testing.py
@@ -1,1 +1,7 @@
-print(oct(10))
+with open('decodeOutput.txt', 'r') as file:
+    data = file.read().split('\n')
+
+data = data[::-1]
+
+with open('decodeOutput.txt', 'w') as file:
+    file.write('\n'.join(data))
